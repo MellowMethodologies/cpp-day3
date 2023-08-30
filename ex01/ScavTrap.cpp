53e40b8f621f810eb110da52aabd7eb36ceb7237
MellowMethodologies <badrsaadarc@gmail.com>
2023-08-30 18:48:18 +0100

    iheritance

--- ex01/ScavTrap.cpp
+++ ex01/ScavTrap.cpp
@@ -6,22 +6,22 @@ ScavTrap::ScavTrap( std::string name ) : ClapTrap(name)
     energy = 50;
     dmg = 20;
 
-    std::cout << "< ScavTrap > - " << this->name << " constructed." << std::endl;
+    std::cout << "ScavTrap " << this->name << " added." << std::endl;
 }
 
 ScavTrap::~ScavTrap() {
-    std::cout << "< ScavTrap > - " << this->name << " destructed." << std::endl;
+    std::cout << "ScavTrap " << this->name << " destructed." << std::endl;
 }
 
 void    ScavTrap::attack( std::string const& target ) {
     if ( this->energy <= 0 ) {
-        std::cout << "< ScavTrap > - " << this->name << " is out of energy." << std::endl;
+        std::cout << " out of energy." << std::endl;
         return;
     }
-    std::cout << "< ScavTrap > - " << this->name << " attacks " << target << " at range, causing " << this->dmg << " points of damage !" << std::endl;
+    std::cout << "ScavTrap " << this->name << " attacks " << target <<"causing " << this->dmg << " points of damage !" << std::endl;
     this->energy -= 1;
 }
 
 void    ScavTrap::guardGate() {
-    std::cout << "< ScavTrap > - " << this->name << " is now in Gate keeper mode." << std::endl;
+    std::cout << "ScavTrap " << this->name << " GateKeeper mode On." << std::endl;
 }
